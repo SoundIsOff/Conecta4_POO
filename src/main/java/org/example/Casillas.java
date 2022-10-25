037f0858c74441aecf2e13b75b1b7e82e109a748
paula <paula.wmarch@alumnos.upm.es>
2022-10-25 14:39:15 +0200

    Constructor, getter, setter clase Casillas

---
+++ src/main/java/org/example/Casillas.java
@@ -0,0 +1,35 @@
+package org.example;
+import java.awt.*;
+public class Casillas {
+    private Ficha ficha;
+    private final int fila;
+    private final int columna;
+    private final String CASILLAVACIA="   ";
+
+    public Casillas(Ficha ficha, int fila, int columna) {
+        this.ficha = ficha;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public Casillas(int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) {
+        this.ficha = ficha;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+}
