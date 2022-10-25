bcef391b72205422040ccf79e2603e8b210fee38
paula <paula.wmarch@alumnos.upm.es>
2022-10-25 18:47:01 +0200

    Constructor, getter, setter, clase Jugador

--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -1,4 +1,39 @@
 package org.example;
+import java.util.Scanner;
 
 public class Jugador {
+    private final char letraficha;
+    private Tablero tablero;
+    private String nombre;
+
+    public Jugador(char letraficha, Tablero tablero, String nombre) {
+        this.letraficha = letraficha;
+        this.tablero = tablero;
+        this.nombre = nombre;
+    }
+
+    public Jugador(char letraficha, String nombre) {
+        this.letraficha = letraficha;
+        this.nombre = nombre;
+    }
+
+    public char getLetraficha() {
+        return letraficha;
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(Tablero tablero) {
+        this.tablero = tablero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
