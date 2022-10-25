6c7c5239c832a82ae1b4d680615693f512342536
paula <paula.wmarch@alumnos.upm.es>
2022-10-25 19:28:36 +0200

    Juego

--- src/main/java/org/example/Conecta4.java
+++ src/main/java/org/example/Conecta4.java
@@ -15,5 +15,30 @@ public class Conecta4 {
         actualizaTableroEnJugadores(this.tablero);
     }
 
+    private void actualizaTableroEnJugadores(Tablero tablero){
+        for(int indice=INICIOBUCLE;indice<NUMJUGADORES;indice++){
+            jugadores[indice].setTablero(tablero);
+        }
+    }
 
+    private Jugador cambiarTurno(){
+        turno.cambiaTurno();
+        return turno.tieneturno();
+    }
+
+    private boolean finPartida(){
+        return tablero.tableroLleno();
+    }
+
+    public void jugar(){
+        Jugador juega=turno.tieneturno();
+        tablero.dibujar();
+
+        do{
+            juega.poner(turno.nombreJugadorConTurno());
+            tablero.dibujar();
+            juega=cambiarTurno();
+        }while(!finPartida());
+        System.out.println("Fin de partida");
+    }
 }
