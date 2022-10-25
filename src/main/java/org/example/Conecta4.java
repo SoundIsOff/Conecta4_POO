73472f8e22acc17a481ddc2a18b9d78e288fde90
paula <paula.wmarch@alumnos.upm.es>
2022-10-25 19:20:53 +0200

    constructor Clase conecta4

--- src/main/java/org/example/Conecta4.java
+++ src/main/java/org/example/Conecta4.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class Conecta4 {
+    private Turno turno;
+    private Jugador[] jugadores;
+    private Tablero tablero;
+    private final int NUMJUGADORES=2;
+    private final int INICIOBUCLE=0;
+    private static final String BIENVENIDA="--- CONECTA 4 ---";
+
+    public Conecta4(Jugador[] jugadores) {
+        this.jugadores = jugadores;
+        this.tablero = tablero;
+        this.turno = new Turno(this.jugadores);
+        actualizaTableroEnJugadores(this.tablero);
+    }
+
+
 }
