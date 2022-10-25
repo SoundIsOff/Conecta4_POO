cfee79cf07592f489fc65b3855892371fb4b9bee
paula <paula.wmarch@alumnos.upm.es>
2022-10-25 15:44:07 +0200

    Constructor, getter, setter clase Tablero

--- src/main/java/org/example/Tablero.java
+++ src/main/java/org/example/Tablero.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Tablero {
+    private Casillas[][] casillas;
+    private final int numfilas;
+    private final int numcolumnas;
+
+    private final int INICIO=0;
+    private final String CADENASEPARADORA="_____________________________";
+    private final String TEXTOCABECERA="--- CONECTA 4 ---";
+    private final String SEPARADORCOLUMNA="|";
+
+    public Tablero(int numfilas, int numcolumnas) {
+        this.casillas = new Casillas[this.numfilas][this.numcolumnas];
+        this.numfilas = numfilas;
+        this.numcolumnas = numcolumnas;
+    }
+
+    public int getNumfilas() {
+        return numfilas;
+    }
+
+    public int getNumcolumnas() {
+        return numcolumnas;
+    }
 }
