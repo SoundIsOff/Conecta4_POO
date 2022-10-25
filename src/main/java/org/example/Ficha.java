6b042d7c7be53001e53e4a64ecabddef98656c41
paula <paula.wmarch@alumnos.upm.es>
2022-10-25 15:25:39 +0200

    Constructor, getter, clase Ficha

---
+++ src/main/java/org/example/Ficha.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Ficha {
+    private final char contenido;
+
+    public Ficha(char contenido) {
+        this.contenido = contenido;
+    }
+
+    public char getContenido() {
+        return contenido;
+    }
+}
