667b0557556d23c022002693b6c26ac0a973455f
Liam <guillermo.esp.jim@gmail.com>
2022-11-23 00:35:47 +0100

    Comentarios en Conecta4

--- src/main/java/org/example/Conecta4.java
+++ src/main/java/org/example/Conecta4.java
@@ -1,5 +1,9 @@
 package org.example;
 
+
+/**
+ * Clase Conecta4 principal
+ */
 public class Conecta4
 {
     private Turno turno;
@@ -9,8 +13,11 @@ public class Conecta4
     private final int INICIOBUCLE = 0;
     private static final String BIENVENIDA = "--- CONECTA 4 ---";
 
-    //Constructor de la clase en el que se define el tamaño del tablero
-    //Necesita el vector con los dos jugadores que van a jugar
+    /**
+     * Constructor de la clase en el que se define el tamaño del tablero
+     * Necesita el vector con los dos jugadores que van a jugar
+     * @param jugadores array de jugadores
+     */
     public Conecta4(Jugador[] jugadores) {
         this.jugadores = jugadores;
         this.tablero = new Tablero(6, 7);
@@ -18,18 +25,25 @@ public class Conecta4
         actualizaTableroEnJugadores(this.tablero);
     }
 
-    //Método que actualiza el tablero para que ambos jugadores puedan colocar fichas
+    /**
+     * Actualiza el tablero para que ambos jugadores puedan colocar fichas
+     * @param tablero objeto tablero que contiene casillas
+     */
     private void actualizaTableroEnJugadores (Tablero tablero) {
         for (int indice = INICIOBUCLE; indice < NUMJUGADORES; indice++) {
             jugadores[indice].setTablero(tablero);
         }
     }
 
-    //Método que gestiona el desarrollo de una partida
-    //El juego termina cuando el tablero está lleno
+    /**
+     * Gestiona el desarrollo de una partida.
+     * Mientras no se dé condición de fin de partida, se pide ficha
+     * al jugador, se dibuja el tablero, se valora el estado de la partida y se cambia de jugador
+     * El juego termina cuando el tablero está lleno o un jugador hace 4 en raya
+     */
     public void jugar() {
         Jugador juega = turno.tieneTurno();
-        boolean fin = false;
+        boolean fin;
         tablero.dibujar();
         do {
             juega.poner(turno.nombreJugadorConTurno());
@@ -39,11 +53,22 @@ public class Conecta4
         } while (!fin);
     }
 
+    /**
+     * Devuelve el nuevo jugador después de alternar entre ellos
+     * @return jugador siguiente en el array de jugadores
+     */
     private Jugador cambiarTurno () {
         turno.cambiaTurno();
         return turno.tieneTurno();
     }
 
+    /**
+     * Se evalúa un boolean que será true si el jugador ha ganado o si el tablero está lleno.
+     * Si no, devolverla false por defecto. En el caso de terminarse la partida, escribe
+     * el mensaje de Final correspondiente
+     * @param jugador se comprueba si este jugador ha ganado
+     * @return boolean con dos casos: true si ha ganado o tablero lleno y false si no
+     */
     private boolean finPartida(Jugador jugador) {
         boolean fin=false;
         if(tablero.compruebaGanador(jugador.getLetraficha())){
