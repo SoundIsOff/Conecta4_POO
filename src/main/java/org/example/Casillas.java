5ff37510a5ad3e9454f9d985848da8038b604989
Liam <guillermo.esp.jim@gmail.com>
2022-11-22 19:47:20 +0100

    Comentarios en Jugador

--- src/main/java/org/example/Casillas.java
+++ src/main/java/org/example/Casillas.java
@@ -1,24 +1,38 @@
 package org.example;
 
+/**
+ * Clase Casilla que se utiliza para formar el tablero.
+ * La fila ni la columna serán inalterables al crearlo.
+ * Cada casilla contendrá una ficha que será null de forma base (devolverá un string vacío). Se le otorgará valor
+ * al asignarle un objeto ficha creado en el método poner() de la clase Jugador.
+ */
 public class Casillas {
     private Ficha ficha;
     private final int fila;
     private final int columna;
     private final String CASILLAVACIA = "   ";
 
+    /**
+     * Constructor de Casillas que inicia el parámetro Ficha a null
+     * fila y columna no cambiarán una vez asignados un valor
+     * @param fila  int fila del tablero en la que está c+la casilla
+     * @param columna int columna del tablero en la que está c+la casilla
+     */
     public Casillas(int fila, int columna) {
         this.ficha = null;
         this.fila = fila;
         this.columna = columna;
     }
 
-
+    /**
+     * Si en la casilla hay ficha sin instanciar, devuelve una ficha vacía
+     * @return ficha objeto ficha
+     */
     public Ficha getFicha() {
         if(ficha!=null)
             return ficha;
         else return new Ficha();
     }
-
 
     /**
      * Asigna la ficha dada y su contenido a la casilla
@@ -28,6 +42,7 @@ public class Casillas {
         this.ficha = ficha;
 
     }
+
     public int getFila() {
         return fila;
     }
@@ -36,15 +51,17 @@ public class Casillas {
         return columna;
     }
 
-    //Método que devuelve si la casilla está ocupada
+    /**
+     * Devuelve si la casilla está vacía u ocupada por una ficha
+     * @return boolean true si ficha en la casilla es null y false si ficha ha sido instanciada
+     */
     public boolean isVacia() {
         return ficha==null;
     }
 
     /**
-     * Método que devuelve la representación de una casilla si esta no está vacía
-     * @return String "vacío"
-     * @return contenido casilla
+     * Devuelve la representación de una casilla si esta no está vacía
+     * @return String "vacío" o el contenido de ficha en caso de que esta no sea null
      */
     public String dibujarCasillas() {
         if (isVacia()) {
--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -2,26 +2,44 @@ package org.example;
 
 import java.util.Scanner;
 
+/**
+ * Clase que representa a los jugadores de una partida
+ * cada jugador tiene un nombre, una letra que representa su ficha, un tablero asignado
+ */
 public class Jugador {
     private final char letraficha;
     private Tablero tablero;
     private String nombre;
 
-    //Constructor que crea un jugador
+    /**
+     * Constructor que crea un jugador
+     * @param nombre cadena de caracteres para el nombre del jugador
+     * @param letra char con la letra que le corresponde al jugador dentro del tablero
+     */
     public Jugador(String nombre, char letra) {
         this.nombre = nombre;
         this.letraficha = letra;
         this.tablero = null;
     }
-
+    /**
+     * metodo para saber el nombre del jugador
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * metodo para cambiar el nomre del jugador
+     * @param nombre cadena de caracteres para el nombre del jugador
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * metodo para conocer la letra que le corresponde al jugador
+     * @return letraficha devuelve la letra
+     */
     public char getLetraficha() {
         return letraficha;
     }
@@ -34,8 +52,11 @@ public class Jugador {
         this.tablero = tablero;
     }
 
-    //Crea la ficha y la asigna al tablero
-    //Método para poner una ficha en una casilla del tablero
+    /**
+     * Crea la ficha y la asigna al tablero
+     * Método para poner una ficha en una casilla del tablero
+     * @param nombreJugador nombre del jugador con el cual se va a colocar la ficha
+     */
     public void poner(String nombreJugador) {
         Scanner scanner = new Scanner(System.in);
         Ficha mificha = new Ficha(this.letraficha);
