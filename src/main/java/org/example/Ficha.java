f8a8dec3318ed657c2d7971731ba71c8dcdaf7b7
bq0178 <ionelagabriela.buhai@alumnos.upm.es>
2022-11-25 09:19:12 +0100

    comentarios clase ficha

--- src/main/java/org/example/Ficha.java
+++ src/main/java/org/example/Ficha.java
@@ -1,11 +1,22 @@
 package org.example;
 
+/**
+ * Clase que crea ficha para conecta4
+ */
 public class Ficha {
     private final char contenido;
 
+    /**
+     * Constructor que crea una ficha con un caracter dado.
+     * @param caracter carácter a mostrar en la ficha.
+     */
     public Ficha (char caracter) {
         this.contenido = caracter;
     }
+
+    /**
+     * constructor para crear una ficha vacia.
+     */
     public Ficha () {
         this.contenido = ' ';
     }
@@ -14,7 +25,10 @@ public class Ficha {
         return contenido;
     }
 
-    //Método que devuelve la representación de una ficha
+    /**
+     * Método que devuelve la representación de una ficha.
+     * @return devuelve el contenido de la ficha.
+     */
     public String dibujarFicha () {
         return " " + contenido + " ";
     }
