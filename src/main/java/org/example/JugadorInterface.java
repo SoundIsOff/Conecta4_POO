ebb2e324235e326f12a658227f2eb831d89ad498
Ionela <ionelagabriela.buhai@alumnos.upm.es>
2022-12-18 13:33:57 +0100

    comentarios en clase JugadorInterface y Menu.

--- src/main/java/org/example/JugadorInterface.java
+++ src/main/java/org/example/JugadorInterface.java
@@ -1,6 +1,11 @@
 package org.example;
 
+/**
+ * Interfaz de Jugador.
+ * Define el comportamiento de las clases de los Judadores persona y CPU.
+ */
 public interface JugadorInterface {
+
     void ponerFicha(String nombreJugador);
 
     String getNombre();
--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -2,17 +2,29 @@ package org.example;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
+/**
+ * Clase que contiene el menú del juego.
+ */
 public class Menu {
     private static final String BIENVENIDA = "--- CONECTA 4 ---";
     private static final String FIN = "--- FIN DE PARTIDA ---\nGracias por jugar!";
     Scanner teclado = new Scanner(System.in);
 
+    /**
+     * Pregunta si se desea continuar jugando tras cada partida.
+     * @return si la respuesta es 's' devuelve true.
+     */
     public boolean continuarPartida(){
         System.out.println("\nPulse \"s\" para continuar.");
         char opcion = teclado.next().charAt(0);
         return opcion == 's';
     }
 
+    /**
+     * Imprime el menú del juego y permite elegir una opción.
+     * @return devuelve la opción elegida, que es el modo de juego.
+     */
     public int elegirModo(){
         int opcion=-1;
         do {
